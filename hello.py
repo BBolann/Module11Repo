84096b6c3de1162e50a9ef305ff616d510a8d871
BBolann <74985854+BBolann@users.noreply.github.com>
2026-04-26 17:56:53 -0400

    Added a new message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # Nolan Farr 
 # 4/26/2026
-#A simple Python program to print a message 
+# A simple Python program to print a message 
+
+#Nolan Farr
+# 4/26/2026
+# added new message
 
 print("Hello, World!")
+
+print("Welcome to GitHub!")
